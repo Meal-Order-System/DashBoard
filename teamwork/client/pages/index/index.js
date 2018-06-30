0fd5babcdd8f43c80ee6feb994d91bae3d0a4374
CCzer <jjkhhu@vip.qq.com>
2018-07-01 01:16:53 +0800

    Update index.js README.md

--- teamwork/client/pages/index/index.js
+++ teamwork/client/pages/index/index.js
@@ -2,9 +2,12 @@
 //获取应用实例
 const app = getApp()
 Page({
+  globalData: {
+    userInfo: null
+  },
   data: {
     motto: '点餐',
-    userInfo: {},
+    nickName: null,
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
@@ -28,42 +31,133 @@ Page({
     })
   },
 
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true,
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        console.log(res.userInfo);
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
+  getUserInfo: function () {
+    var openId = (wx.getStorageSync('openId'))
+    var that = this;
+    if (openId) {
       wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+        success: function (res) {
+          console.log(res);
+          that.setData({
+            nickName: res.userInfo.nickName,
+            avatarUrl: res.userInfo.avatarUrl,
+            hasUserInfo: true,
           })
+        },
+        fail: function () {
+          // fail
+          console.log("获取失败！")
+        },
+        complete: function () {
+          // complete
+          console.log("获取用户信息完成！")
         }
       })
+    } else {
+      wx.login({
+        success: function (res) {
+          console.log(res.code)
+          if (res.code) {
+            wx.getUserInfo({
+              withCredentials: true,
+              success: function (res_user) {
+                wx.request({
+                  //后台接口地址
+                  url: 'https://meal.mlg.kim/user/login',
+                  data: {
+                    code: res.code,
+                    encryptedData: res_user.encryptedData,
+                    iv: res_user.iv
+                  },
+                  method: 'GET',
+                  header: {
+                    'content-type': 'application/json'
+                  },
+                  success: function (res) {
+                    console.log(res);
+                    // this.globalData.userInfo = JSON.parse(res.data);
+                    wx.getUserInfo({
+                      success: function (res) {
+                        console.log(res);
+                        that.setData({
+                          nickName: res.userInfo.nickName,
+                          avatarUrl: res.userInfo.avatarUrl,
+                          hasUserInfo: true,
+                        })
+                      },
+                      fail: function () {
+                        // fail
+                        console.log("获取失败！")
+                      },
+                      complete: function () {
+                        // complete
+                        console.log("获取用户信息完成！")
+                        console.log(that.data)
+                      }
+                    })
+                  }
+                })
+              }, fail: function () {
+                wx.showModal({
+                  title: '警告通知',
+                  content: '您点击了拒绝授权,将无法正常显示个人信息,点击确定重新获取授权。',
+                  success: function (res) {
+                    if (res.confirm) {
+                      wx.openSetting({
+                        success: (res) => {
+                          if (res.authSetting["scope.userInfo"]) {////如果用户重新同意了授权登录
+                            wx.login({
+                              success: function (res_login) {
+                                if (res_login.code) {
+                                  wx.getUserInfo({
+                                    withCredentials: true,
+                                    success: function (res_user) {
+                                      wx.request({
+                                        url: 'https://meal.mlg.kim/user/login',
+                                        data: {
+                                          code: res_login.code,
+                                          encryptedData: res_user.encryptedData,
+                                          iv: res_user.iv
+                                        },
+                                        method: 'GET',
+                                        header: {
+                                          'content-type': 'application/json'
+                                        },
+                                        success: function (res) {
+                                          that.setData({
+                                            nickName: res.data.nickName,
+                                            avatarUrl: res.data.avatarUrl,
+
+                                          })
+                                          wx.setStorageSync('openId', res.data.openId);
+                                        }
+                                      })
+                                    }
+                                  })
+                                }
+                              }
+                            });
+                          }
+                        }, fail: function (res) {
+
+                        }
+                      })
+
+                    }
+                  }
+                })
+              }, complete: function (res) {
+
+
+              }
+            })
+          }
+        }
+      })
+
     }
   },
 
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+  
+  
 })
